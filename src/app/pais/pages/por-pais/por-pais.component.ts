c79cac0a4dbca6649cd3bb25da871828fc10a006
lzamranoa <lzambranoa.software@gmail.com>
2021-10-24 20:10:38 -0500

    Creacion del servicio para buscar por pais y del entorno html del mismo

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+  termino: string = "Colombia"
+
+  constructor( private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    console.log(this.termino);
+
+    this.paisService.buscarPais( this.termino )
+      .subscribe((resp) => {
+        console.log(resp);
+      })
   }
 
 }
